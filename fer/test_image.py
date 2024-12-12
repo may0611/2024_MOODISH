0c2507762502674c372e94c913059531ec274495
Lim_Dong_Hyeon <151642091+saru033@users.noreply.github.com>
2024-12-12 16:31:56 +0900

    api화 (임시)

--- fer/test_image.py
+++ fer/test_image.py
@@ -1,18 +1,23 @@
 from fer import FER
 import cv2
 
-img_path = r"C:\Users\sny79\test_image.jpg"
+img_path = r"C:\Users\user\Documents\GitHub\2024_MOODISH\fer\test_image.jpg"
 img = cv2.imread(img_path)
 
-if img is None:
-    print("이미지를 불러오지 못했습니다. 경로를 확인하세요.")
-else:
-    detector = FER()
-    results = detector.detect_emotions(img)
 
-    if not results:
-        print("얼굴을 탐지하지 못했거나 감정을 분석하지 못했습니다.")
+
+def Emotion(img):
+    if img is None:
+        print("이미지를 불러오지 못했습니다. 경로를 확인하세요.")
     else:
-        for face in results:
-            print(f"탐지된 얼굴 위치: {face['box']}")
-            print(f"감정 분석 결과: {face['emotions']}")
+        detector = FER()
+        results = detector.detect_emotions(img)
+
+        if not results:
+             print("얼굴을 탐지하지 못했거나 감정을 분석하지 못했습니다.")
+        else:
+            for face in results:
+                print(f"탐지된 얼굴 위치: {face['box']}")
+                print(f"감정 분석 결과: {face['emotions']}")
+
+    return results
--- scanning_receipt/receipt.py
+++ scanning_receipt/receipt.py
@@ -2,30 +2,37 @@ import pytesseract
 from PIL import Image
 from konlpy.tag import Okt
 
-# Tesseract 경로 설정
-pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
-
-# 이미지 로드
-image = Image.open('./image/receipt1.png')
-
-# OCR을 사용하여 텍스트 추출
-text = pytesseract.image_to_string(image, lang='kor')
 
 # ingredients.txt 파일에서 식재료 목록 읽기
 def load_ingredients(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         return [line.strip() for line in f.readlines()]
 
-# 파일에서 식재료 목록을 읽어옴
-ingredients_list = load_ingredients('listOfIngredients.txt')
+  # 이미지 로드
+#image = Image.open('./image/receipt1.png')
 
-# 한국어 형태소 분석
-okt = Okt()
-words = [word for word, pos in okt.pos(text) if pos == 'Noun']
+def receipt_function(image):
 
-# 추출된 식재료 필터링
-extracted_ingredients = [word for word in words if word in ingredients_list]
+    # Tesseract 경로 설정
+    pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
 
-# 추출된 식재료 출력
-print("추출된 식재료:", extracted_ingredients)
-print(text)
+
+    # OCR을 사용하여 텍스트 추출
+    text = pytesseract.image_to_string(image, lang='kor')
+
+
+    # 파일에서 식재료 목록을 읽어옴
+    ingredients_list = load_ingredients('listOfIngredients.txt')
+
+    # 한국어 형태소 분석
+    okt = Okt()
+    words = [word for word, pos in okt.pos(text) if pos == 'Noun']
+
+    # 추출된 식재료 필터링
+    extracted_ingredients = [word for word in words if word in ingredients_list]
+
+    # 추출된 식재료 출력
+        #print("추출된 식재료:", extracted_ingredients)
+        #print(text)
+    return extracted_ingredients
+
