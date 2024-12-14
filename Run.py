bf7e1261ec7ec87af6c20627b4cf6db319960f2c
May Lee <leece611>
2024-12-14 21:51:58 +0900

    충돌처리

--- Run.py
+++ Run.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas as pd
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import Recommend
+from data import Recommend
+from fer import test_fer
+from scanning_receipt import receipt
 
 
 #cd pra , npm start
@@ -23,9 +25,9 @@ user_preferences = {
     'sad' : 0,
 }
 
-
-@app.route('/process', methods=['POST'])
-def process_data():
+#추천 시스템
+@app.route('/recommend', methods=['POST'])
+def recommend_data():
     data = request.get_json()  # 클라이언트에서 보낸 JSON 데이터 받기
     user_input_ingre = data.get('userInput')  # 사용자가 보낸 입력값 받기
     user_input_time = data.get('userInput_time')
@@ -42,6 +44,26 @@ def process_data():
     print(joined_string)
     print("request check")
     return jsonify({'result': joined_string})
+
+#감정 분석
+@app.route('/emtion', methods=['POST'])
+def emtion_data():
+    data = request.get_json()  # 이미지 형태 (json아닐수도 있음)
+
+    result = test_fer.Emotion(data)
+
+    result2 = result['emotions']
+
+    return jsonify({'emotion': result2}) # return type에 따라 추후 변경
+
+#영수증 인식
+@app.route('/receipt', methods=['POST'])
+def receipt_data():
+    data = request.get_json()  # 이미지 형태 (json아닐수도 있음)
+
+    result = receipt.receipt_function(data)
+
+    return jsonify({'receipt': result}) # return type에 따라 추후 변경
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port = 5000)
