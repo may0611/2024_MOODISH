a8af0f4efe34e05b040ddee982826b6ad278e2f0
may0611 <mayLEE202237783@gmail.com>
2024-12-15 18:30:26 +0900

    add food_img

--- Run.py
+++ Run.py
@@ -7,6 +7,7 @@ from fer import test_fer
 from scanning_receipt import receipt
 from PIL import Image
 import io
+from food_img import info
 
 
 #cd pra , npm start
@@ -65,6 +66,18 @@ def recommend_data():
     print(joined_string)
     print("request check")
     return jsonify({'result': joined_string})
+
+
+#사진 이미지 반환
+@app.route('/foodImg', methods=['POST'])
+def receipt_data():
+    data = request.get_json()  #음식 이름
+    foodName_get = data.get('foodName')
+
+    res = info.food_info(foodName_get)
+
+    return jsonify({'img_link': res}) # return type에 따라 추후 변경
+
 #감정 분석
 @app.route('/analyze_emotion', methods=['POST'])
 def analyze_emotion():
